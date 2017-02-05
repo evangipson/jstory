c1988c051dda90e528539927302fe45e2b2590d6
Evan Gipson <evangipson@gmail.com>
2017-02-05 02:06:31 -0600

    Remove javascript obfuscation
    It is giving me trouble running the javascript.
    
    Also cleaned up the gulp file to reflect this removal.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var sass = require("gulp-sass");
 var uglify = require("gulp-uglify");
 var babel = require("gulp-babel");
 var es2015 = require('babel-preset-es2015');
-var obfuscate = require("gulp-obfuscate");
 
 // General log function for gulp
 gulp.task("log", function(message) {
@@ -14,10 +13,10 @@ gulp.task("log", function(message) {
 
 // All the logs we'll use
 var logs = {
-    introMessage: " === Running gulp to build SASS and JavaScript === ",
+    introMessage: " === Running Gulp to build SASS and JavaScript === ",
     sass: " === Finished building SCSS into css/style.css === ",
-    javascript: " === Obfuscating and minifying javascript into js/jstory.js === ",
-    endMessage: " === Done running gulp! === "
+    javascript: " === Concatening and minifying JavaScript into js/jstory.js === ",
+    endMessage: " === Done running Gulp! === "
 };
 
 gulp.task("begin", function() {
@@ -37,7 +36,6 @@ gulp.task("javascript", function() {
     gulp.src("src/js/*.js")
     .pipe(babel({ presets: [es2015] }))
     .pipe(concat("jstory.js"))
-    .pipe(obfuscate())
     .pipe(uglify().on('error', function(e){
          console.log(e);
     }))
